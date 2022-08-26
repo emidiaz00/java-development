e8df0b344ed8e54b30eb4e42eb39514443a3d9a9
emidiaz00 <emildsn00@gmail.com>
2022-08-26 12:12:07 -0300

    agrego proyecto

---
+++ src/HolaMundo.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+
+        double miArreglo[] = new double[6];
+        int contador = 0;
+
+
+        for (int i = 0; i < miArreglo.length; i++) {
+            miArreglo[i] = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la califación " + (i + 1)));
+        }
+
+        System.out.println("Mostrar califaciones aprobadas: ");
+
+        for (int i = 0; i < miArreglo.length; i++) {
+            if (miArreglo[i] >= 6) {
+                System.out.println(miArreglo[i]);
+                contador++;
+            }
+        }
+    }
+
+}
