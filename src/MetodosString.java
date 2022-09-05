5ec801cae17ad804ed73a280581c810352feb653
emidiaz00 <emildsn00@gmail.com>
2022-09-05 15:22:12 -0300

    agrego operadores asignacion

--- src/MetodosString.java
+++ src/MetodosString.java
@@ -6,7 +6,15 @@ public class MetodosString {
         System.out.println("nombre = " + nombre.toUpperCase());
         System.out.println("nombre.toLowerCase() = " + nombre.toLowerCase());
         System.out.println("nombre.equals(\"emiliano\") = " + nombre.equals("emiliano"));
+        System.out.println("nombre.charAt(1) = " + nombre.charAt(1));
+        System.out.println("nombre.substring(1) = " + nombre.substring(1));
 
-        
+        String trabalengua = "trabalenguas";
+        System.out.println("nombre = " + trabalengua.replace("a", "A"));
+        System.out.println("trabalengua.indexOf('a') = " + trabalengua.indexOf('a'));
+        System.out.println("trabalengua.lastIndexOf('a') = " + trabalengua.lastIndexOf('a'));
+
+
+    
     }
 }
---
+++ src/OperadoresAsignacion.java
@@ -0,0 +1,11 @@
+public class OperadoresAsignacion {
+    public static void main(String[] args) {
+        int a = 5;
+        int j = a + 4;
+        // operadores combinados o compuestos
+        
+        a = a + 2;
+        a +=2;
+        System.out.println("a = " + a);
+    }
+}
