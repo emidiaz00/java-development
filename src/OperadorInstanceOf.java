7bf8364500738049e959e5bb4b2019d9f225ad59
emidiaz00 <emildsn00@gmail.com>
2022-09-05 21:10:24 -0300

    agrego op ternario

---
+++ src/OperadorInstanceOf.java
@@ -0,0 +1,7 @@
+public class OperadorInstanceOf {
+    public static void main(String[] args) {
+
+        String texto = "Creando un objeto de la clase string";
+        Integer num = 2;
+    }
+}
---
+++ src/OperadorTernario.java
@@ -0,0 +1,6 @@
+public class OperadorTernario {
+    public static void main(String[] args) {
+        String variable = 5 == 5 ? "es igual" : "es diferente";
+        System.out.println(variable);
+    }
+}
